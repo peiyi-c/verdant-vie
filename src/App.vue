7b345e96b1b022f9fecd6ff3fba4ace6ea611313
peiyi-c <pennieyoyoyo@gmail.com>
2024-04-19 21:50:49 +0200

    add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <HelloWorld />
+  <Navbar />
   <router-view></router-view>
 </template>
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: { Navbar },
 };
 </script>
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap");
+
+body {
+  font-family: "Lato", "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+    sans-serif;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+      <a href="/">
+        <img src="../../public/logo.svg" alt="logo vie" class="" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarItems"
+        aria-controls="navbarItems"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarItems">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link text-dark" to="/all">ALL </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-dark" to="/hair"
+              >HAIR</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-dark" to="/face"
+              >FACE</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-dark" to="/body"
+              >BODY</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-dark" to="/home"
+              >HOME
+            </router-link>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search..."
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-primary rounded-1" type="submit">
+            <span class="material-symbols-outlined"> search </span>
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script></script>
+
+<style></style>
