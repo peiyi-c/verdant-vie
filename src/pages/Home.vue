81ed3d1e60d356a0c78b08a2747a4db69096a5a3
peiyi-c <pennieyoyoyo@gmail.com>
2024-04-19 21:52:50 +0200

    add home component

---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-5">
+    <button class="btn btn-primary">Home</button>
+    <button class="btn btn-secondary">Home</button>
+    <button class="btn btn-dark">Home</button>
+    <button class="btn btn-info">Home</button>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
