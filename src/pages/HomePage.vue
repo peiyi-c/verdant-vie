2cda09aa38e932de12945dea8ebd6d72ea16cada
peiyi-c <pennieyoyoyo@gmail.com>
2024-04-24 16:47:14 +0200

    add homepage placeholder components

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,31 @@
 <template>
   <h1 class="text-center">Home</h1>
+  <!-- products -->
+  <section class="mt-4 row justify-content-evenly">
+    <ProductCard v-for="item in hair" :key="item.id" :item="item" />
+    <ProductCard v-for="item in body" :key="item.id" :item="item" />
+    <ProductCard v-for="item in face" :key="item.id" :item="item" />
+    <ProductCard v-for="item in home_style" :key="item.id" :item="item" />
+    <ProductCard v-for="item in accessory" :key="item.id" :item="item" />
+  </section>
 </template>
 
 <script>
+import ProductCard from "../components/ProductCard.vue";
+
 export default {
   name: "HomePage",
+  inject: ["products"],
+  components: { ProductCard },
+  data() {
+    return {
+      accessory: this.products.accessory,
+      body: this.products.body,
+      hair: this.products.hair,
+      face: this.products.face,
+      home_style: this.products.home_style,
+    };
+  },
 };
 </script>
 
