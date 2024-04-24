398be3a74e871108cfd71226a4b45d061f8a7960
peiyi-c <pennieyoyoyo@gmail.com>
2024-04-24 14:30:20 +0200

    add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer
+    class="container w-100 mt-5 px-5 py-3 d-flex flex-column flex-md-row justify-content-start justify-content-lg-between border border-1 border-red"
+  >
+    <div class="">
+      <p>INFO</p>
+      <ul>
+        <li>Our Story</li>
+        <li>Orders</li>
+        <li>Customer Service</li>
+        <li>FAQs</li>
+        <li>Store</li>
+      </ul>
+    </div>
+    <div class="">
+      <p>Cooperation</p>
+      <ul>
+        <li>Crossover</li>
+      </ul>
+    </div>
+    <div class="">
+      <p>Contact Us</p>
+      <ul>
+        <li>Address</li>
+      </ul>
+    </div>
+    <div class="">
+      <p>Follow Us</p>
+    </div>
+  </footer>
+</template>
+
+<style>
+footer ul {
+  list-style: none;
+}
+</style>
