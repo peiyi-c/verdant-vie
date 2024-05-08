3e9fd50a4c77441632cdf5d376fe9b777b88dcff
peiyi-c <pennieyoyoyo@gmail.com>
2024-05-08 10:26:38 +0200

    adjust alignment

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,35 @@
 <template>
-  <footer
-    class="container w-100 mt-5 px-5 py-3 d-flex flex-column flex-md-row justify-content-start justify-content-lg-between border border-1 border-red"
-  >
-    <div class="">
-      <p>INFO</p>
-      <ul>
-        <li>Our Story</li>
-        <li>Orders</li>
-        <li>Customer Service</li>
-        <li>FAQs</li>
-        <li>Store</li>
-      </ul>
-    </div>
-    <div class="">
-      <p>Cooperation</p>
-      <ul>
-        <li>Crossover</li>
-      </ul>
-    </div>
-    <div class="">
-      <p>Contact Us</p>
-      <ul>
-        <li>Address</li>
-      </ul>
-    </div>
-    <div class="">
-      <p>Follow Us</p>
+  <footer class="w-100 mt-5 border border-1 border-red bg-light-subtle">
+    <div
+      class="container px-5 py-3 d-flex flex-column flex-lg-row justify-content-start justify-content-lg-between"
+    >
+      <div class="">
+        <p class="fw-bold">INFO</p>
+        <ul class="lh-lg">
+          <li>Our Story</li>
+          <li>Orders</li>
+          <li>Customer Service</li>
+          <li>FAQs</li>
+          <li>Store</li>
+        </ul>
+      </div>
+      <div class="">
+        <p class="fw-bold">Cooperation</p>
+        <ul class="lh-lg">
+          <li>Crossover</li>
+        </ul>
+      </div>
+      <div class="">
+        <p class="fw-bold">Contact Us</p>
+        <p class="ms-1 lh-base">
+          Verdant Street 17<br />
+          New Silbermond City<br />
+          Nebelland
+        </p>
+      </div>
+      <div class="">
+        <p class="fw-bold">Follow Us</p>
+      </div>
     </div>
   </footer>
 </template>
@@ -33,5 +37,7 @@
 <style>
 footer ul {
   list-style: none;
+
+  padding-left: 0.25rem;
 }
 </style>
