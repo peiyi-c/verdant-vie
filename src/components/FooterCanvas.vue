1bf79bd334620b0de48cfeb6fca037e8d1d59996
peiyi-c <pennieyoyoyo@gmail.com>
2024-05-08 13:28:49 +0200

    add footer canvas

---
+++ src/components/FooterCanvas.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="offcanvas offcanvas-bottom bg-info-subtle rounded-top-2 p-4 p-lg-5"
+    tabindex="-1"
+    id="offcanvasFooter"
+    aria-labelledby="offcanvasFooterLabel"
+  >
+    <div class="offcanvas-header w-100 d-flex justify-content-between">
+      <h5 class="offcanvas-title" id="offcanvasFooterLabel">Our Story</h5>
+      <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="offcanvas-body small">...</div>
+  </div>
+</template>
+<style lang="scss"></style>
