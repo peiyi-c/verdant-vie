e3a46a8a9647e86736c0e0e0dace731ba71cd771
peiyi-c <pennieyoyoyo@gmail.com>
2024-05-14 15:05:13 +0200

    adjust shopping bag position in larger screens

--- src/components/ShoppingBag.vue
+++ src/components/ShoppingBag.vue
@@ -2,8 +2,9 @@
   <div class="position-absolute shopping-bag mx-2">
     <div class="position-relative wrapper">
       <div class="text-center text-lg-end">
-        <button
-          class="btn btn-sm shopping-bag__btn"
+        <div
+          role="button"
+          class="shopping-bag__btn"
           data-bs-toggle="offcanvas"
           data-bs-target="#shoppingCart"
           aria-controls="shoppingCart"
@@ -19,7 +20,7 @@
               d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z"
             />
           </svg>
-        </button>
+        </div>
       </div>
       <!-- cart items quantity badge -->
       <span
@@ -52,9 +53,17 @@ export default {
 </script>
 
 <style lang="scss">
+@use "../scss/utility/" as *;
 .shopping-bag {
-  top: 1.1rem;
+  top: 1.2rem;
   right: 1.5rem;
+  @include breakpoint-up(medium) {
+    top: 1.3rem;
+    right: 2rem;
+  }
+  @include breakpoint-up(large) {
+    right: 2.1rem;
+  }
   &__btn {
     &:focus,
     &:active {
